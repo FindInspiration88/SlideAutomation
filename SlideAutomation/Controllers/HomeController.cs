c39be9fffad3398393e4544fde6ea39a4bc4f32f
sunny_bs <47714578+sunnybs@users.noreply.github.com>
2019-03-08 21:49:48 +0500

    Загрузка фонов в zip / рефакторинг / фикс директорий през

--- SlideAutomation/Controllers/HomeController.cs
+++ SlideAutomation/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using SlideAutomation.Models;
 using System.IO;
 using System.Text;
 using System.Drawing;
+using System.IO.Compression;
+
 
 namespace SlideAutomation.Controllers
 {
@@ -24,37 +26,54 @@ namespace SlideAutomation.Controllers
             var presentationId = GetPresentationId();
             var presentationDir = Server.MapPath("~/Presentations/" + presentationId);
             CreatePresentationDir(presentationDir);
-
-
-
             var textsFile = upload.ToArray()[0];
             var backgroundsFile = upload.ToArray()[1];
+            if (backgroundsFile != null && backgroundsFile.FileName.Contains(".zip"))
+            {
+                ExtractBackgrounds(presentationDir, backgroundsFile);
+            }
+            if (textsFile != null && textsFile.FileName.Contains(".txt"))
+            {
+                CreateSlidesFromTxt(presentationDir, textsFile);
+            }
+            return Redirect("/Home/Slide/0/" + presentationId);
+        }
 
-            if (textsFile != null)
-                {
-                    var fileName = Path.GetFileName(textsFile.FileName);
-                    var filePath = presentationDir + "/" + fileName;
-                    textsFile.SaveAs(filePath);    
-                    if (fileName.Contains(".txt"))    
-                        {
-                            StreamReader fileStream = new StreamReader(filePath, Encoding.Default);
-                            var slideTexts = fileStream.ReadToEnd()
-                                .Split(new string[] { "СЛАЙД: ", "СЛАЙД:" }, StringSplitOptions.RemoveEmptyEntries);
-                            var slides = new List<Slide>();
-                            foreach(var slideText in slideTexts)
-                            {
-                                var slide = new Slide();
-                                slide.Title = slideText.Split('\n')[0];
-                                slide.Content = slideText.Replace(slide.Title, "");
-                                slide.PathToBackgroundFile = GetRandomBackground();
-                                slides.Add(slide);
-                            }
-                            CreateSlides(slides);
-                        }        
-                    }
+        private void ExtractBackgrounds(string presentationDir, HttpPostedFileBase backgroundsFile)
+        {
+            var fileName = Path.GetFileName(backgroundsFile.FileName);
+            var filePath = presentationDir + "/" + fileName;
+            backgroundsFile.SaveAs(filePath);
+           try
+           {
+                ZipFile.ExtractToDirectory(filePath, presentationDir + "/Backgrounds");
+           }
+           catch (Exception e)
+           {
+                System.IO.File.Copy(Server.MapPath("~/Files/default.jpg"), presentationDir + "/Backgrounds/default.jpg");
+           }
+
+        }
+
+        private void CreateSlidesFromTxt(string presentationDir, HttpPostedFileBase textsFile)
+        {
             
-            return Redirect("/Home/Slide/" + int.Parse(presentationId) + "/0");
-            //return Redirect("/Home/Slide/");
+            var fileName = Path.GetFileName(textsFile.FileName);
+            var filePath = presentationDir + "/" + fileName;
+            textsFile.SaveAs(filePath);
+            StreamReader fileStream = new StreamReader(filePath, Encoding.Default);
+            var slideTexts = fileStream.ReadToEnd()
+                .Split(new string[] { "СЛАЙД: ", "СЛАЙД:" }, StringSplitOptions.RemoveEmptyEntries);
+            var slides = new List<Slide>();
+            foreach (var slideText in slideTexts)
+            {
+                var slide = new Slide();
+                slide.Title = slideText.Split('\n')[0];
+                slide.Content = slideText.Replace(slide.Title, "");
+                slide.PathToBackgroundFile = GetRandomBackground(presentationDir);
+                slides.Add(slide);
+            }
+            SaveSlides(slides, presentationDir);
         }
 
         private void CreatePresentationDir(string path)
@@ -67,9 +86,11 @@ namespace SlideAutomation.Controllers
             }
         }
 
-        private string GetRandomBackground()
+        private string GetRandomBackground(string presentationDir)
         {
-            var BackgroundPaths = Directory.GetFiles(Server.MapPath("~/Files/Backgrounds"));
+            var BackgroundPaths = Directory.GetFiles(presentationDir + "/Backgrounds");
+            BackgroundPaths = BackgroundPaths
+               .Where(path => path.Contains(".png") || path.Contains(".jpg")).ToArray();
             return BackgroundPaths[new Random().Next(BackgroundPaths.Length)];
         }
 
@@ -90,26 +111,27 @@ namespace SlideAutomation.Controllers
        [HttpGet]
         public ActionResult Slide(int id, string name)
         {
-            var slidePaths = Directory.GetFiles(Server.MapPath("~/Home/Slide/Slides/"));
-            var slides = slidePaths.Select(path => "Slides/" + Path.GetFileName(path)).ToList();
+            var slidePaths = Directory.GetFiles(Server.MapPath("~/Presentations/" + name + "/Slides")).ToList();
+            var slides = slidePaths
+              .Select(path => "~/Presentations/" + name + "/Slides/" + Path.GetFileName(path)).ToList();
             ViewBag.Slides = slides;
             return View();
         }
 
        
 
-        private void CreateSlides(List<Slide> slides)
+        private void SaveSlides(List<Slide> slides, string presentationDir)
         {
             for (var i = 0; i<slides.Count;i++)
             {
-                Image picture = Image.FromFile(slides[i].PathToBackgroundFile); //получаем исходное изображение из файла 
-                var pictureToOurFormat = new Bitmap(picture, 800, 500); // форматируем изображение 
-                Graphics part2 = Graphics.FromImage(pictureToOurFormat); //получаем его часть 
+                Image picture = Image.FromFile(slides[i].PathToBackgroundFile);  
+                var pictureToOurFormat = new Bitmap(picture, 800, 500); 
+                Graphics part2 = Graphics.FromImage(pictureToOurFormat); 
                 part2.DrawString(slides[i].Content,
-                new System.Drawing.Font("Arial", 26, FontStyle.Regular),
+                new Font("Arial", 26, FontStyle.Regular),
                 new SolidBrush(Color.WhiteSmoke), new RectangleF(100, 100, 400, 500),
-                new StringFormat(StringFormatFlags.NoClip)); // наносим на эту часть текст с параметрами 
-                pictureToOurFormat.Save(Server.MapPath("~/Home/Slide/Slides/") + i.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);//записываем получающееся изображение в файл
+                new StringFormat(StringFormatFlags.NoClip));
+                pictureToOurFormat.Save(presentationDir + "/Slides/" + i.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);//записываем получающееся изображение в файл
             }
         }
 
